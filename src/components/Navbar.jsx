896c85a5c47df22ecee3c5d6bb47aaa0adace725
Isara <tito.arun96@gmail.com>
2023-09-19 23:03:09 +0930

    Initial Page pre color overhaul

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -15,11 +16,31 @@ const Navbar = () => {
 
             {/* Menu */}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                    <Link to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             {/* Hamburger */}
@@ -30,11 +51,31 @@ const Navbar = () => {
             {/* Mobile Menus */}
             <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
                 <ul>
-                    <li className='py-6 text-4xl'>Home</li>
-                    <li className='py-6 text-4xl'>About</li>
-                    <li className='py-6 text-4xl'>Skills</li>
-                    <li className='py-6 text-4xl'>Work</li>
-                    <li className='py-6 text-4xl'>Contact</li>
+                    <li className='py-6 text-4xl'>
+                        <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='py-6 text-4xl'>
+                        <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                            About
+                        </Link>
+                    </li>
+                    <li className='py-6 text-4xl'>
+                        <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                            Skills
+                        </Link>
+                    </li>
+                    <li className='py-6 text-4xl'>
+                        <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                            Work
+                        </Link>
+                    </li>
+                    <li className='py-6 text-4xl'>
+                        <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                            Contact
+                        </Link>
+                    </li>
                 </ul>
             </div>
 
---
+++ src/components/Skills.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Placeholder from '../assets/jesse.png'
+
+const Skills = () => {
+    return (
+        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+                    <p className='py-4'>These are the technologies and tools I am familiar with.</p>
+                </div>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='Jesse' />
+                        <p className='my-4'>Jesse</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='Team Fight Tactics' />
+                        <p className='my-4'>Team Fight Tactics</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='Leadership' />
+                        <p className='my-4'>Leadership</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='Flaming' />
+                        <p className='my-4'>Flaming</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='Blaming' />
+                        <p className='my-4'>Blaming</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='CSGO' />
+                        <p className='my-4'>CSGO</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='SQL' />
+                        <p className='my-4'>SQL</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Placeholder} alt='AWS' />
+                        <p className='my-4'>AWS</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Skills
