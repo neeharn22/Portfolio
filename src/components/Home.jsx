74b654f3b90a71d0cff3e1e71d9c8361e30f143c
Isara <tito.arun96@gmail.com>
2023-09-20 20:52:25 +0930

    Added typewriter-effect module

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll';
+import Typewriter from 'typewriter-effect'
 
 const Home = () => {
     return (
-        <div name="home" className='w-full h-screen bg-[#0a192f]'>
+        <div name="home" className='w-full h-screen bg-[#000C66]'>
 
             {/*Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-pink-600'>Hi, my name is</p>
-                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Neehar N</h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Data Analyst</h2>
+                <p className='text-[#a5a5a5]'>Hi, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ffffff]'>Neehar Namjoshi</h1>
+                <div className='text-4xl sm:text-7xl font-bold text-[#8892b0] flex'>
+                    <h2>I am a </h2>
+                    <h2 className='ml-4'>{<Typewriter options={{ strings: ['Data Analyst.', 'Mech. Engineer.', 'Patent Engineer.', 'QA Engineer.', 'SQL Analyst.', 'Apex Legend.', 'Dog Leader.', 'Degenerate.'], autoStart: true, loop: true }} />}</h2>
+                </div>
                 <p className='text-[#8892b0] py-4 max-w-[700px] '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae obcaecati id, eos magni veniam natus beatae, necessitatibus exercitationem dolores dicta rerum mollitia tempore deserunt consequuntur hic nesciunt ab eveniet nam!</p>
                 <div>
                     <Link to="work" smooth={true} duration={500}>
-                        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#050A30] hover:border-[#050A30]'>
                             View Work
                             <span className='group-hover:rotate-90 duration-300'>
                                 <HiArrowNarrowRight className='ml-3' />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav);
 
     return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#050A30] text-white'>
             <div>
                 NN logo
             </div>
